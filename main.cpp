4be17c7e898e41c64f6cfe731a382e5c089b13b1
HaiduculRo <115576512+HaiduculRo@users.noreply.github.com>
2023-03-19 23:41:25 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,31 +2,46 @@
 #include <fstream>
 #include <chrono>
 using namespace std;
-ifstream f("1-10^3 aporape random.txt");
+ifstream f("citire.txt");
 ofstream g("radix.out");
-int n,sortat[100000006];
-int shellSort(int sortat[], int n)
-{   for (int salt=n/2; salt>=1; salt/=2)
-    {   for (int i=salt; i<=n; i++)
-        {   int temp=sortat[i];
-            int j;
-            for (j=i; j>=salt && sortat[j-salt] > temp; j-=salt)
-                sortat[j] = sortat[j-salt];
-            sortat[j] = temp;
+int x,buckets[1000000],maxx,sortat[100000002],aux[100000002];
+void radix(int k,int sortat[], int maxim, int n)
+{   int p=1;
+    while(maxim)
+    {   ///int k=10;
+        for(int i=1;i<=n;++i)
+            ++ buckets[sortat[i]/p%k];
+
+        for(int i=1;i<k;++i)
+            buckets[i] += buckets[i-1];
+        for(int i=n; i>=1;--i)
+        {   int poz = sortat[i]/p%k;
+            aux[buckets[poz]] = sortat[i];
+            buckets[poz] --;
         }
+
+        for(int i=1;i<=n;++i)
+            sortat[i] = aux[i];
+        for(int i=0;i<k;++i)
+            buckets[i] = 0;
+        maxim/=k;
+        p*=k;
     }
-    return 0;
 }
+
 int main()
-{   int n,x;
+{   int n,k;
+    cin>>k;
     f>>n;
-     for(int i=1; i<=n; i++)
+    int maxx=-1;
+    for(int i=1; i<=n; i++)
     {   f>>x;
+        if(maxx < x)
+            maxx = x;
         sortat[i] = x;
     }
-
     auto start = chrono::steady_clock::now();
-    shellSort(sortat,n);
+    radix(k,sortat,maxx,n);
     auto end = chrono::steady_clock::now();
 
      cout << "Elapsed time in seconds: "
@@ -44,3 +59,15 @@ int main()
 
     return 0;
 }
+
+
+/**
+ int a=61;
+    int b=16;
+    unsigned int numar = 0b11010110;
+    unsigned int ultimii_4_biti = numar & 0b1111;
+    cout<< ultimii_4_biti<<'\n';
+    int c = a & 16;
+    cout<<c;
+*/
+
